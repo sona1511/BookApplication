c7f1da0df2a42fdfd49ddb4e9368a8efb5dca82c
sona1511 <sacyadav2005@gmail.com>
2021-02-27 12:43:57 +0530

    Second Commit

--- BookApplication/src/com/sona/corejava/bookapp5/BookService.java
+++ BookApplication/src/com/sona/corejava/bookapp5/BookService.java
@@ -1,37 +1,43 @@
-package com.sona.corejava.bookapp5;
-public class BookService  //6
-{
-	                           //   100      corejava       250   
-	public int addBookService(int bookId,String title,double price) //7
-	{  System.out.println("I am in Service Layer..");  //8
-		String grade="";
-		if(price<=300)
-		{
-			grade="C";
+	package com.sona.corejava.bookapp5;
+	public class BookService  //6
+	{
+		                           /**
+		                            *  100      corejava       250   
+		                            * @param bookId
+		                            * @param title
+		                            * @param price
+		                            * @return
+		                            */
+		public int addBookService(int bookId,String title,double price) //7
+		{  System.out.println("I am in Service Layer..");  //8
+			String grade="";
+			if(price<=300)
+			{
+				grade="C";
+				}
+			else if(price<=600)
+			{
+				grade="B";
 			}
-		else if(price<=600)
-		{
-			grade="B";
+			else
+			{
+				grade="A"; }
+			BookDAO bookDAO=new BookDAO(); // 8 
+			BookBean bookBean=new BookBean(); //9
+			         bookBean.setBookId(bookId);
+			         bookBean.setTitle(title);
+			         bookBean.setPrice(price);
+			         bookBean.setGrade(grade);
+			    //15
+			int getValueDAO=bookDAO.addBook(bookBean); //10
+			
+			
+			
+			
+			return 1;
+			
+			
+			
 		}
-		else
-		{
-			grade="A"; }
-		BookDAO bookDAO=new BookDAO(); // 8 
-		BookBean bookBean=new BookBean(); //9
-		         bookBean.setBookId(bookId);
-		         bookBean.setTitle(title);
-		         bookBean.setPrice(price);
-		         bookBean.setGrade(grade);
-		    //15
-		int getValueDAO=bookDAO.addBook(bookBean); //10
-		
-		
-		
-		
-		return 1;
-		
-		
-		
+	
 	}
-
-}
